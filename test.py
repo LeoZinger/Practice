c6e34295c56478da2684eed2053f4f338323bf19
Zinger <lzinger@3c063007183d.ant.amazon.com>
2022-09-11 22:16:17 -0700

    GTech Coding Test - highest Avg SAT score

---
+++ test.py
@@ -0,0 +1,27 @@
+sat_list = [{"company": "google", "name":"tom", "sat": 98}, {"company": "google", "name": "greg", "sat": 95},
+            {"company": "meta", "name": "ian", "sat": 98}, {"company": "meta", "name": "darren", "sat": 82},
+            {"company": "amazon", "name": "tom", "sat": 97}, {"company": "amazon", "name": "nathan", "sat": 91},
+            {"company": "microsoft", "name": "sam", "sat": 92}, {"company": "microsoft", "name": "peter", "sat": 98},
+            ]
+
+
+print(sat_list)
+sat_res = {}
+sat_count = {}
+
+# sat_res["google"] = (str(sat_list[0]["sat"]), 1)
+# print(sat_res)
+
+for sat_map in sat_list:
+    count = sat_count.get(sat_map["company"], 0) + 1
+    print("Count = ", count)
+    sat_count[sat_map["company"]] = count
+    sat_res[sat_map["company"]] = (sat_res.get(sat_map["company"], (0, 0))[0] + sat_map["sat"], count)
+    print(sat_res)
+
+    sat_res_list = sorted(sat_res.items(), key=lambda x: x[1][0], reverse=True)
+    print(sat_res_list)
+    print("best SAT score: ", sat_res_list[0])
+
+
+
